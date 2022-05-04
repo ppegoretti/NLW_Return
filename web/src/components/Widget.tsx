4d52c3e2aebbeb4c644e923dbfd94d896355a2fa
ppegoretti <ppegoretti@hotmail.com>
2022-05-04 12:35:19 -0300

    finishi class 1

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,9 +1,29 @@
 import { ChatTeardropDots} from 'phosphor-react'
+import { useState } from 'react'
+import { Popover } from '@headlessui/react'
+
+//Estado
+
 
 export function Widget(){
+    // const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+    // function toggleWidgetVisibility(){
+    //     setIsWidgetOpen(!isWidgetOpen)
+    // }
+
     return (
-    <button className="bg-brand-500 rounded-full px-3 h-12 text-white">
-        <ChatTeardropDots className='w-6 h6'/>
-    </button>
+    <Popover className='absolute bottom-4 right-4'>
+        <Popover.Panel>Hello World</Popover.Panel>
+        {/* {isWidgetOpen ? <p>Hello World</p> : null} */}
+        {/* {isWidgetOpen && <p>Hello World</p> } */}
+            <Popover.Button  className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+                <ChatTeardropDots className='w-6 h6'/>
+                <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+                    <span className='pl-2'></span>
+                    Feedback
+                </span>
+            </Popover.Button>
+    </Popover>
         )
 }
