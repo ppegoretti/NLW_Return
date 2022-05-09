7885789992eeb68fa1536448d46748600be61de0
ppegoretti <ppegoretti@hotmail.com>
2022-05-09 12:51:32 -0300

    class 2 1:10:11

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 
 import { CloseButton } from "../CloseButton";
 
-import bugImageUrl from '../assets/bug.svg'
-import ideaImageUrl from '../assets/idea.svg'
-import thoughtImageUrl from '../assets/thought.svg'
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
+import { FeedbackTypeStep } from "./Steps/FeedBackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
     BUG:{
         title: 'Problema',
         image: {
@@ -30,36 +32,27 @@ const feedbackTypes = {
     },
 }
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
 
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
+    function handleRestartFeedback(){
+        setFeedbackType(null)
+    }
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>
-                <CloseButton />
-            </header>
+           
 
             {!feedbackType ? (
-                <div className="flex py-8 gap-2 w-full">
-                {Object.entries(feedbackTypes).map(([key, {image, title}]) => (
-                    <button
-                        key={key}
-                        className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                        onClick={() => setFeedbackType(key as FeedbackType)}
-                        type="button"
-                    >
-                        <img src={image.source} alt={image.alt}></img>
-                        <span>{title}</span>
-                    </button>
-                )
-                )}
-            </div>
+                <FeedbackTypeStep onFeedbackTypeTypeChanged={setFeedbackType}/>
             ):(
-                <p>Hello Would</p>
+                <FeedbackContentStep 
+                    feedbackType={feedbackType}
+                    onFeedbacRestartRequested={handleRestartFeedback}
+                />
             )
         }
 
